6e6589d158c6f107d91a169a780aecdddcfbff6f
Iago de Souza Lima <iago.souza069@gmail.com>
2018-07-14 15:18:44 -0300

    binding por interpolação page Feed

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -14,6 +14,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'feed.html',
 })
 export class FeedPage {
+  public nome_usuario:string = "Iago de Souza";
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
